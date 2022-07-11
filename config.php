aebd033880fb563da3fd96623846319bad2470fd
Andreas Hartono <80753860+andreasHartono@users.noreply.github.com>
2022-07-11 07:32:41 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
 // Configure your Datumbox API Key. Get yours at http://www.datumbox.com/apikeys/view/
-define('DATUMBOX_API_KEY', '0b1b4d6ad91d3058f31a163fdd7c838f');
+define('DATUMBOX_API_KEY', '');
 
 // Configure your OAuth settings from your application at https://dev.twitter.com/apps
 
-define('TWITTER_CONSUMER_KEY', '4gzSwZZJZKJd9RqqrMTe9n2LO');
-define('TWITTER_CONSUMER_SECRET', '9LGU0C3FeRvt4jROz0n6L6LPeajt7muWaOfnzFBjQGx3VwYuum');
+define('TWITTER_CONSUMER_KEY', '');
+define('TWITTER_CONSUMER_SECRET', '');
 
 
 // Configure authentication credentials.
 // you can generate your own access key from the link above
 
-define('TWITTER_ACCESS_KEY', '1448552621845856260-gOg9ZIDTjJ9f1aWWqjTMcDDdqxc6nc');
-define('TWITTER_ACCESS_SECRET', 'nH3DUXbFM9kO9CAqgReNDbLeQjJ7TZ1Hd34GHN0CQEczu');
+define('TWITTER_ACCESS_KEY', '');
+define('TWITTER_ACCESS_SECRET', '');
